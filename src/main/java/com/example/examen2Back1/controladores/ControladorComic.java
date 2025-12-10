4988366aefab02bf62ea5ca0a1322aaa030fcc88
mariamontezuma15 <montezumagonz@gmail.com>
2025-12-10 11:40:38 -0500

    entidaddes

--- src/main/java/com/example/examen2Back1/controladores/ControladorComic.java
+++ src/main/java/com/example/examen2Back1/controladores/ControladorComic.java
@@ -1,4 +1,9 @@
 package com.example.examen2Back1.controladores;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/comicapi/v1/comic")
 public class ControladorComic {
 }
--- src/main/java/com/example/examen2Back1/controladores/ControladorEmpleado.java
+++ src/main/java/com/example/examen2Back1/controladores/ControladorEmpleado.java
@@ -1,4 +1,10 @@
 package com.example.examen2Back1.controladores;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/comicapi/v1/empleado")
 public class ControladorEmpleado {
+
 }
