da425c71f7280c7786bfbe854ead97db0efdb5aa
Javier David Martinez Arrieta <javier.martinez02@globant.com>
2025-12-10 12:22:24 -0500

    Guardando cambios

--- src/main/java/com/example/examen2Back1/controladores/ControladorEmpleado.java
+++ src/main/java/com/example/examen2Back1/controladores/ControladorEmpleado.java
@@ -1,10 +1,24 @@
 package com.example.examen2Back1.controladores;
 
+import com.example.examen2Back1.modelos.Empleado;
+import com.example.examen2Back1.servicios.ServicioEmpleado;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/comicapi/v1/empleado")
 public class ControladorEmpleado {
+    @Autowired
+    ServicioEmpleado servicioEmpleado;
+    @PostMapping
+    public ResponseEntity<Empleado>guardar(@RequestBody Empleado datos){
+        Empleado respuestaDelApi=this.servicioEmpleado.guardarEmpleado(datos);
+        return ResponseEntity.status(HttpStatus.OK).body(respuestaDelApi);
+    }
 
 }
